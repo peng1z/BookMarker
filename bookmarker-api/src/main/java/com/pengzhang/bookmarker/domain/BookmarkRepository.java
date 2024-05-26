0dce7a1e07973ea5af945802b1d60238f9f47410
Patrick Zhang <pzh1pzh@gmail.com>
2024-05-26 13:31:13 -0700

    Build JPA entity, repo, service

---
+++ bookmarker-api/src/main/java/com/pengzhang/bookmarker/domain/BookmarkRepository.java
@@ -0,0 +1,10 @@
+package com.pengzhang.bookmarker.domain;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+
+
+}
---
+++ bookmarker-api/src/main/java/com/pengzhang/bookmarker/domain/BookmarkService.java
@@ -0,0 +1,18 @@
+package com.pengzhang.bookmarker.domain;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class BookmarkService {
+    private final BookmarkRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<Bookmark> getBookmarks(Integer page) {
+    }
+}
